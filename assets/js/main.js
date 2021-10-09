13bb2a9b8136b76b6a63dc24b0062fa55247ee69
Anurag Rajan <anuragrajanp@gmail.com>
2021-10-09 13:14:58 +0530

    D2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', ()=>{
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('nav-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu')
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('nav-menu')
     })
 }
 
